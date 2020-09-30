495cedb72ee6a9f3ff9c404bfad806117d63c949
LondonComputadores <london.computadores@gmail.com>
2020-09-29 23:08:38 -0400

    CREATED AND REMOVED: created new json file named lawsuits_okay replacing lawsuit.json. Verify opened issue! Removed unused app named referral.

--- referrals/models.py
+++
@@ -1,8 +0,0 @@
-from django.db import models
-from court_app.models import Lawsuit
-
-class Referral(models.Model):
-    numero = models.ForeignKey(Lawsuit, on_delete=models.CASCADE)
-
-    def __str__(self):
-        return self.numero
